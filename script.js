fd724b3aec175fdd923d0ea7f18d483576dce880
AGN <itzme907@gmail.com>
2023-05-10 22:21:58 +0300

    Add playGame function
    Start the game with two parameters (computerSelection, playerSelection)
    and return a string containing the winner based on nested if else
    statments.

--- script.js
+++ script.js
@@ -5,7 +5,21 @@ const getComputerChoice = () => {
     return computerChoices[rand];
 }
 
+function playGame(computerSelection, playerSelection) {
 
+    if (computerSelection === playerSelection) {
+        return 'It\'s a tie!';
+    } else if (computerSelection === 'rock' && playerSelection === 'scissors') {
+        return 'Computer win!';
+    } else if (computerSelection === 'paper' && playerSelection === 'rock') {
+        return 'Computer win!';
+    } else if (computerSelection ==='scissors' && playerSelection === 'paper') {
+        return 'Computer win!';
+    } else {
+        return 'Player win!';
+            }
+
+        }
 
 
 
